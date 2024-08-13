e7a74fa859a48dd265b6c1e443e71c36b6ed66bb
shiho-k <shii0327@yahoo.co.jp>
2024-08-13 16:17:13 +0900

    Lesson 18Chapter 10.7 プログラムの実行（入力チェック）

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,7 +37,10 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            return getRegister(user);
+        }
         service.saveUser(user);
         return "redirect:/user/list";
     }
--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -1,5 +1,7 @@
 package com.techacademy.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -17,21 +24,29 @@ public class User {
     public static enum Gender {
         男性, 女性
     }
-/**主キー。自動生成*/
+
+    /** 主キー。自動生成 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max = 20)
     private String name;
 
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 
+    @Min(0)
+    @Max(120)
     private Integer age;
 
     @Column(length = 50)
+    @Email
+    @Length(max = 50)
     private String email;
 
 }
